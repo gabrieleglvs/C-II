cd48610a8b431007e412b41c50e2f4ccf3fbf8cc
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2022-11-05 19:18:54 -0300

    imprimindo um array de caracteres com spritf

--- forca.c
+++ forca.c
@@ -3,14 +3,11 @@
 int main() {
     char palavrasecreta[20];
 
-    palavrasecreta[0] = 'M';
-    palavrasecreta[1] = 'E';
-    palavrasecreta[2] = 'L';
-    palavrasecreta[3] = 'A';
-    palavrasecreta[4] = 'N';
-    palavrasecreta[5] = 'C';
-    palavrasecreta[6] = 'I';
-    palavrasecreta[7] = 'A';
+    sprintf(palavrasecreta, "MELANCIA");
+    /*1º parâmetro: variável ou array
+    2º parâmetro: palavra ou frase
+    Essa função irá guardar o que vc escreveu na var ou array...
+    - ela encherga o array de char como uma string, então só vai para de exibir o que está lá dentro, quando encontrar um final, que logicamente é 'vazio \0', então se tenho uma frase no array e quero exibir apenas uma palavra da frase, eu coloco o \0 no indice logo após o termino da palavra*/
 
-    printf("%C%C%C%C%C%C%C%C", palavrasecreta[0], palavrasecreta[1], palavrasecreta[2], palavrasecreta[3], palavrasecreta[4], palavrasecreta[5], palavrasecreta[6], palavrasecreta[7]);
+    printf("%s", palavrasecreta);
 }
