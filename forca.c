af8b3bc072fc012bac269f6c1b0e6d6828607313
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2022-11-05 18:48:16 -0300

    começando com ARRAYS

--- forca.c
+++ forca.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    char letra1 = 'M';
-    char letra2 = 'E';
-    char letra3 = 'L';
-    char letra4 = 'A';
-    char letra5 = 'N';
-    char letra6 = 'C';
-    char letra7 = 'I';
-    char letra8 = 'A';
+    int notas[10];
 
-    printf("%C%C%C%C%C%C%C%C", letra1, letra2, letra3, letra4, letra5, letra6, letra7, letra8);
+    notas[0] = 10;
+    notas[2] = 9;
+    notas[3] = 8;
+    notas[9] = 4;
+
+    printf("notas %d %d %d", notas[0], notas[2], notas[9]);
 }
