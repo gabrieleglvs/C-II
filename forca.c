ebebaebcf16b4eee2a2a48c32936ce32bbde2513
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2022-11-06 12:12:20 -0300

    loop do-while

--- forca.c
+++ forca.c
@@ -4,10 +4,12 @@ int main() {
     char palavrasecreta[20];
 
     sprintf(palavrasecreta, "MELANCIA");
-    /*1º parâmetro: variável ou array
-    2º parâmetro: palavra ou frase
-    Essa função irá guardar o que vc escreveu na var ou array...
-    - ela encherga o array de char como uma string, então só vai para de exibir o que está lá dentro, quando encontrar um final, que logicamente é 'vazio \0', então se tenho uma frase no array e quero exibir apenas uma palavra da frase, eu coloco o \0 no indice logo após o termino da palavra*/
 
-    printf("%s", palavrasecreta);
+    int acertou = 0;
+    int enforcou = 0;
+
+    do {
+        //começar o nosso jogo!!
+    } while (!acertou && !enforcou);
+    //vamos trocar o == 0 para !acertou, que se lê 'não acertou' ou 'não enforcou'   
 }
