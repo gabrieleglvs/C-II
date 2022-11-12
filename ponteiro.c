3797d3875abbfb3e71547f6e45960d0be202189d
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2022-11-12 18:20:10 -0300

    funcionamento de var com mesmo nome do main na função

--- ponteiro.c
+++ ponteiro.c
@@ -1,1 +1,23 @@
 //aprendendo sobre ponteiros
+
+#include <stdio.h>
+
+void calcula(int c) {
+    printf("calcula %d\n", c);
+    c++;
+    printf("calcula %d\n", c);
+}
+
+int main() {
+    int c = 10;
+
+    printf("main %d\n", c);
+    calcula(c);
+    printf("main %d\n", c);
+}
+
+//impressão:
+//main 10
+//calcula 10
+//calcula 11
+//main 10
