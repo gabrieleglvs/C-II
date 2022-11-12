fb13c2ad9ea5d56e347473eff97e979ddf81d881
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2022-11-12 18:46:06 -0300

    apontando endereço de memoria

--- ponteiro.c
+++ ponteiro.c
@@ -1,21 +1,20 @@
 //Para que a var c, seja a mesma tanto no main quanto na funcao, o endereço de memória dela tem que aportar para o mesmo lugar, tanto na funcao, quanto no main.
 
-//Para verificar o endereço de memórial de cada variável eu uso o & antes da variavel.
-
 #include <stdio.h>
 
-void calcula(int c) {
-    //imprimindo o endereço de memória das vars
-    printf("calcula %d %d\n", c, &c);
-    c++;
-    printf("calcula %d %d\n", c, &c);
+//coloquei um asterisco para sinalizar que o que estou passando é o endereço de memória da var c que está no main.
+void calcula(int* c) {
+
+    //o asterisco antes do c, declara que eu quero entrar no endereço de memoria e pegar o valor que tem lá dentro, e depois soma +1.
+    printf("calcula %d %d\n", (*c), c);
+    (*c)++;
+    printf("calcula %d %d\n", (*c), c);
 }
 
 int main() {
     int c = 10;
 
-    //imprimindo o endereço de memória das vars
     printf("main %d %d\n", c, &c);
-    calcula(c);
+    calcula(&c);
     printf("main %d %d\n", c, &c);
 }
