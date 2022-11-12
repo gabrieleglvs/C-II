5903c00e36569df51e401bf199bef6c656d4f775
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2022-11-12 18:29:42 -0300

    entendendo os ponteiros

--- ponteiro.c
+++ ponteiro.c
@@ -1,23 +1,21 @@
-//aprendendo sobre ponteiros
+//Para que a var c, seja a mesma tanto no main quanto na funcao, o endereço de memória dela tem que aportar para o mesmo lugar, tanto na funcao, quanto no main.
+
+//Para verificar o endereço de memórial de cada variável eu uso o & antes da variavel.
 
 #include <stdio.h>
 
 void calcula(int c) {
-    printf("calcula %d\n", c);
+    //imprimindo o endereço de memória das vars
+    printf("calcula %d %d\n", c, &c);
     c++;
-    printf("calcula %d\n", c);
+    printf("calcula %d %d\n", c, &c);
 }
 
 int main() {
     int c = 10;
 
-    printf("main %d\n", c);
+    //imprimindo o endereço de memória das vars
+    printf("main %d %d\n", c, &c);
     calcula(c);
-    printf("main %d\n", c);
+    printf("main %d %d\n", c, &c);
 }
-
-//impressão:
-//main 10
-//calcula 10
-//calcula 11
-//main 10
